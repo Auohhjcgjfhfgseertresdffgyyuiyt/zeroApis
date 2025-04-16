436fc79563997a3bcfb5c5ddc24c2658a8f42dea
Auohhjcgjfhfgseertresdffgyyuiyt <125882163+Auohhjcgjfhfgseertresdffgyyuiyt@users.noreply.github.com>
2025-04-16 19:14:44 +0700

    Update index.js

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ app.get("/", (req, res) => {
 });
 
 // Dynamic API routes
-const routes = ["igdl", "fbdl", "ttdl", "githubstalk", "searchgroups", "llama-3.3-70b-versatile"];
+const routes = ["igdl", "fbdl", "ttdl", "githubstalk", "searchgroups", "llama-3.3-70b-versatile", "hentavid"];
 routes.forEach((route) => {
     try {
         app.use(`/api/${route}`, require(`./api/${route}`));
